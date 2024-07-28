c47663460b9cce5f9bc684a8db62b52a375aabc6
paulbono <paulbono@users.noreply.github.com>
2024-07-28 15:35:02 -0500

    Update snake.js
    Remove script tags

--- snake.js
+++ snake.js
@@ -1,7 +1,6 @@
-<script>
+
 window.alert("hi");
 var gameCanvas = document.getElementById("game");
 var ctx = gameCanvas.getContext("2d");
 ctx.fillStyle = "blue";
 ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-</script>
