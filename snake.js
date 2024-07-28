1ba6d7c0aec399ad4d5063b0372e3a9bda6b5032
paulbono <paulbono@users.noreply.github.com>
2024-07-28 15:42:49 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,6 +1,9 @@
 
+$( document ).ready(function() {
+    
 window.alert("hi");
 var gameCanvas = document.getElementById("game");
 var ctx = gameCanvas.getContext("2d");
 ctx.fillStyle = "blue";
 ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+});
